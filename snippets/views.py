430d077cf8d6fb66f06db167540c84e8b45d922f
Nicole Aragao <nicolearagao@gmail.com>
2023-04-22 19:35:47 -0300

    refactor: move from funcs to class

--- snippets/views.py
+++ snippets/views.py
@@ -1,19 +1,49 @@
-from rest_framework.decorators import api_view
+from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
+from django.http import Http404
 
-@api_view(["GET", "POST"])
-def snippet_list(request, format=None):
-    """later."""
-    if request.method == "GET":
+
+class SnippetList(APIView):
+    """"""
+    def get(self, request, format=None):
+        """"""
         snippets = Snippet.objects.all()
         serializer = SnippetSerializer(snippets, many=True)
         return Response(serializer.data)
 
-    elif request.method == "POST":
+    def post(self, request, format=None):
+        """"""
         serializer = SnippetSerializer(data=request.data)
+        # request.data can handle other other formats beyond json
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+            # returning response with data but allowing DRF to render into correct type
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)               
+
+
+class SnippetDetail(APIView):
+    """"""
+    def get_object(self, pk):
+        """"""
+        try:
+            return Snippet.objects.get(pk=pk)
+        except Snippet.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        """"""
+        snippet = self.get_object(pk=pk)
+        serializer = SnippetSerializer(snippet)
+        return Response(serializer.data)
+
+    def put(self, request, pk, format=None):
+        """"""
+        snippet = self.get_object(pk=pk)
+        serializer = SnippetSerializer(snippet, data=request.data)
         # request.data can handle other other formats beyond json
         if serializer.is_valid():
             serializer.save()
@@ -21,25 +51,8 @@ def snippet_list(request, format=None):
             # returning response with data but allowing DRF to render into correct type
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-
-@api_view(["GET", "PUT", "DELETE"])
-def snippet_detail(request, pk):
-    """Later."""
-    try:
-        snippet = Snippet.objects.get(pk=pk)
-    except Snippet.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
-
-    if request.method == "GET":
-        serializer = SnippetSerializer(snippet)
-        return Response(serializer.data)
-    elif request.method == "PUT":
-        serializer = SnippetSerializer(snippet, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        else:
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    elif request.method == "DELETE":
+    def delete(self, request, pk, format=None):
+        """"""
+        snippet = self.get_object(pk=pk)
         snippet.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
