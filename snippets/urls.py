bb2e90460297b322a4e9b27a5eb0dbd6180b3499
Nicole Aragao <nicolearagao@gmail.com>
2023-04-22 19:19:50 -0300

    refactor: use api_view and Response on views

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 from snippets import views
+from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
     path("snippets/", views.snippet_list),
     path("snippets/<int:pk>/", views.snippet_detail),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
+# adding this to urls to make sure the api will be able to handle URLs that
+# explicitly refer to a given format
