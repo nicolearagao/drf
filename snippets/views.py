db1ab3a4bf4df281583e61ae066ce5048b978f0d
Nicole Aragao <nicolearagao@gmail.com>
2023-04-22 19:48:30 -0300

    refactor: use generics only

--- snippets/views.py
+++ snippets/views.py
@@ -1,36 +1,15 @@
-from rest_framework import mixins
 from rest_framework import generics
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
 
 
-class SnippetList(generics.GenericAPIView, mixins.CreateModelMixin, mixins.ListModelMixin):
+class SnippetList(generics.ListCreateAPIView):
     """"""
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
 
-    def get(self, request, *args, **kwargs):
-        """"""
-        return self.list(self, request, *args, **kwargs)
 
-    def post(self, request, *args, **kwargs):
-        """"""
-        return self.create(self, request, *args, **kwargs)
-
-
-class SnippetDetail(generics.GenericAPIView, mixins.DestroyModelMixin, mixins.UpdateModelMixin, mixins.RetrieveModelMixin):
+class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     """"""
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
-
-    def get(self, request, *args, **kwargs):
-        """"""
-        self.retrieve(self, request, *args, **kwargs)
-
-    def put(self, request, *args, **kwargs):
-        """"""
-        self.update(self, request, *args, **kwargs)
-
-    def delete(self, request, *args, **kwargs):
-        """"""
-        self.destroy(self, request, *args, **kwargs)
